c4106c3a4a7ef91e8077f4c207ca5fc4f760f65d
skyplane23 <moraruliviunicolae1@gmail.com>
2023-03-02 14:51:10 +0200

    loading performance entry added
    ts update

--- src/vs/code/browser/workbench/preloader/main.js
+++ src/vs/code/browser/workbench/preloader/main.js
@@ -5,24 +5,48 @@
 
 // Dots
 // ====
-const dots = document.querySelecto('.dots');
-
+const dots = document.querySelector('.dots');
 // Function
 // ========
 function animate(element, className) {
-	if (element) {
-		element.classList.add(className);
-		setTimeout(() => {
-			element.classList.remove(className);
+	const preloader = document.getElementById("preloader");
+	if (preloader && window.getComputedStyle(preloader).display !== "none") {
+		if (element) {
+			element.classList.add(className);
 			setTimeout(() => {
-				animate(element, className);
-			}, 500);
-		}, 2500);
-	} else {
-		console.error('.dots elements missing');
+				element.classList.remove(className);
+				setTimeout(() => {
+					animate(element, className);
+				}, 500);
+			}, 2500);
+		} else {
+			console.error(".dots elements missing")
+		}
+	}
+}
+
+function displayPerformanceEntries() {
+	const preloader = document.getElementById("preloader");
+	if (preloader && window.getComputedStyle(preloader).display !== "none") {
+		const preloaderText = document.getElementById("preloader-text");
+		if (preloaderText) {
+			const entries = performance.getEntries()
+			if (entries.length) {
+				preloaderText.innerHTML = `[${entries[entries.length - 1].name}]`;
+			} else {
+				preloaderText.innerHTML = '[]';
+			}
+		}
+
+		setTimeout(() => {
+			displayPerformanceEntries();
+		}, 50);
 	}
 }
 
 // Execution
 // =========
-animate(dots, 'dots--animate');
+animate(dots, "dots--animate");
+displayPerformanceEntries();
+
+//# sourceURL=file:///F:/vscode/src/vs/code/browser/workbench/preloader/main.js
