8bd6cbe1901ffe4c0ee8500b670e34de43612992
Cristian Paris <cristian.tanasep@gmail.com>
2024-03-21 13:47:34 +0200

    Add new loading screen

--- src/vs/code/browser/workbench/preloader/main.js
+++ src/vs/code/browser/workbench/preloader/main.js
@@ -4,52 +4,29 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
-// Dots
-// ====
-const dots = document.querySelector('.dots');
 const maxCount = 2509;
-// Function
-// ========
-function animate(element, className) {
-	const preloader = document.getElementById('preloader');
-	if (preloader && window.getComputedStyle(preloader).display !== 'none') {
-		if (element) {
-			element.classList.add(className);
-			setTimeout(() => {
-				element.classList.remove(className);
-				setTimeout(() => {
-					animate(element, className);
-				}, 500);
-			}, 2500);
-		} else {
-			console.error('.dots elements missing');
-		}
-	}
-}
 
-function displayPerformanceEntries() {
+function displayLoadPercentage() {
 	const preloader = document.getElementById('preloader');
 	if (preloader && window.getComputedStyle(preloader).display !== 'none') {
 		const preloaderText = document.getElementById('preloader-text');
 		if (preloaderText) {
-			let total = performance.getEntriesByType('resource').length;
+			const total = performance.getEntriesByType('resource').length;
 			if (total <= maxCount) {
-				preloaderText.innerHTML = `[${Math.round((total / maxCount) * 100)} %]`;
+				preloaderText.innerHTML = `${Math.round((total / maxCount) * 100)} %`;
 			} else {
-				preloaderText.innerHTML = '[Applying finishing touches. Please be patient]';
+				preloaderText.innerHTML = 'Applying finishing touches. Please be patient';
 			}
 		}
 
 		setTimeout(() => {
-			displayPerformanceEntries();
+			displayLoadPercentage();
 		}, 250);
 	}
 }
 
 // Execution
 // =========
-performance.setResourceTimingBufferSize(2510);
-displayPerformanceEntries();
-animate(dots, 'dots--animate');
-
+performance.setResourceTimingBufferSize(maxCount);
+displayLoadPercentage();
 //# sourceURL=file:///F:/vscode/src/vs/code/browser/workbench/preloader/main.js
