799c39463befc42abd80e9de82c18f7a5aba2cd5
Cristian Paris <cristian.tanasep@gmail.com>
2024-03-21 12:28:29 +0200

    Fix loading screen

--- src/vs/code/browser/workbench/preloader/main.js
+++ src/vs/code/browser/workbench/preloader/main.js
@@ -7,11 +7,12 @@
 // Dots
 // ====
 const dots = document.querySelector('.dots');
+const maxCount = 2509;
 // Function
 // ========
 function animate(element, className) {
-	const preloader = document.getElementById("preloader");
-	if (preloader && window.getComputedStyle(preloader).display !== "none") {
+	const preloader = document.getElementById('preloader');
+	if (preloader && window.getComputedStyle(preloader).display !== 'none') {
 		if (element) {
 			element.classList.add(className);
 			setTimeout(() => {
@@ -21,13 +22,34 @@ function animate(element, className) {
 				}, 500);
 			}, 2500);
 		} else {
-			console.error(".dots elements missing")
+			console.error('.dots elements missing');
 		}
+	}
+}
+
+function displayPerformanceEntries() {
+	const preloader = document.getElementById('preloader');
+	if (preloader && window.getComputedStyle(preloader).display !== 'none') {
+		const preloaderText = document.getElementById('preloader-text');
+		if (preloaderText) {
+			let total = performance.getEntriesByType('resource').length;
+			if (total <= maxCount) {
+				preloaderText.innerHTML = `[${Math.round((total / maxCount) * 100)} %]`;
+			} else {
+				preloaderText.innerHTML = '[Applying finishing touches. Please be patient]';
+			}
+		}
+
+		setTimeout(() => {
+			displayPerformanceEntries();
+		}, 250);
 	}
 }
 
 // Execution
 // =========
-animate(dots, "dots--animate");
+performance.setResourceTimingBufferSize(2510);
+displayPerformanceEntries();
+animate(dots, 'dots--animate');
 
 //# sourceURL=file:///F:/vscode/src/vs/code/browser/workbench/preloader/main.js
